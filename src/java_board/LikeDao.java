982c7b18d54d07398cc21d43b6091b5297f39cd7
jangdo98 <jang09813@gmail.com>
2020-11-04 12:43:17 +0900

    게시판진행중

--- src/java_board/LikeDao.java
+++ src/java_board/LikeDao.java
@@ -1,5 +1,31 @@
 package java_board;
 
-public class LikeDao {
+import java.util.ArrayList;
 
+public class LikeDao {
+	private ArrayList<Like> likes;
+	private int no = 1;
+	
+	public LikeDao() {
+		likes = new ArrayList<>();
+	}
+	
+	public void insertLike(Like like) {
+		like.setId(no);
+		no++;
+		like.setRegDate(Util.getCurrentDate());
+		likes.add(like);
+	}
+	
+	public Like getLikeByArticleIdAndMemberId(int aid, int mid) {
+		
+		for (int i = 0; i < likes.size(); i++) {
+			Like like = likes.get(i);
+			if (like.getParentId() == aid && like.getMemberId() == mid) {
+				return like;
+			}
+		}
+		
+		return null;
+	}
 }
